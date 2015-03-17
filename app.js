5c8a96e5858226f4d012d3df15d55311c08adc77
Devin Torres <devin@devintorr.es>
2015-03-17 17:48:18 -0500

    Add /delay endpoint with a setTimeout (100ms)

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ app.get('/', function(req, res) {
   res.status(204).send();
 });
 
+app.get('/delay', function(req, res) {
+  setTimeout(function() {
+    res.status(204).send();
+  }, 100);
+});
+
 module.exports = app;
